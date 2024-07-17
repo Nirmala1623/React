267f45a52f99d60c416c322684ead81347c6dff7
Nirmala1623 <nirmala.bonala@techgrit.com>
2024-07-17 17:05:49 +0530

    react essentials

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
+        <section id="examples">
+          <h1>Examples</h1>
+          <menu>
+          <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Propes</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
